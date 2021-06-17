a59deab1baafa6174774beb98c55490b7c216308
tapswap <tapswapmoneydex@gmail.com>
2021-06-17 13:58:23 +0800

    chore: dynamic galaxy

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,18 +1,26 @@
+import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
 import { PancakeTheme } from '@pancakeswap-libs/uikit/dist/theme'
+import useTheme from 'hooks/useTheme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
   export interface DefaultTheme extends PancakeTheme {}
 }
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyle = () => {
+  const { isDark } = useTheme()
+  const Style = createGlobalStyle`
   * {
     font-family: 'Kanit', sans-serif;
   }
   body {
+
     background-color: ${({ theme }) => theme.colors.background};
+    background-image: url(${
+      isDark && 'https://cdn.discordapp.com/attachments/847731075640066048/854624011149639681/Galaxy.png'
+    });
 
     img {
       height: auto;
@@ -20,5 +28,12 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 `
+
+  return (
+    <>
+      <Style />
+    </>
+  )
+}
 
 export default GlobalStyle
