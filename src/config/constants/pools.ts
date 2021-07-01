a467fb3f9881c80463534774061903fd3d43a239
tapswap <tapswapmoneydex@gmail.com>
2021-07-01 15:46:59 +0800

    (chore) added smart faucet busd v2

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2,21 +2,21 @@ import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
   {
-    sousId: 67, // V1
+    sousId: 67, // V2
     tokenName: 'BUSD',
-    tokenLabel: 'BUSD v1',
+    tokenLabel: 'BUSD v2',
     tokenAddress: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
     stakingTokenName: QuoteToken.TAPS,
     stakingTokenAddress: '0x56eab07247e3e6404ac90140F20bba61375d5C3C',
     stakingTokenDecimals: 18,
     contractAddress: {
       97: '',
-      56: '0x600a04123C354D0c462C84BB6FdDE51048Ea1Ef9',
+      56: '0x6Bf9e6B8752bED5E8c91A58D93dE8ABD2D9c3804',
     },
     poolCategory: PoolCategory.CORE,
-    projectLink: 'https://tapswap.finance',
+    projectLink: 'https://tapswap.finance/',
     harvest: true,
-    tokenPerBlock: '0.00496031746',
+    tokenPerBlock: '0.0005',
     sortOrder: 999,
     isFinished: false,
     tokenDecimals: 18,
@@ -25,30 +25,30 @@ const pools: PoolConfig[] = [
     bushVersion: 1,
     getUrl: '/#/swape?outputCurrency=0x56eab07247e3e6404ac90140F20bba61375d5C3C',
   },
-  {
-    sousId: 68, // V1
-    tokenName: 'WBNB',
-    tokenLabel: 'WBNB v1',
-    tokenAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    stakingTokenName: QuoteToken.TAPS,
-    stakingTokenAddress: '0x56eab07247e3e6404ac90140F20bba61375d5C3C',
-    stakingTokenDecimals: 18,
-    contractAddress: {
-      97: '',
-      56: '0x705993A5576c67Ba7d0d1B8a39857AeB28712201',
-    },
-    poolCategory: PoolCategory.CORE,
-    projectLink: 'https://www.binance.com',
-    harvest: true,
-    tokenPerBlock: '0.00001388888889',
-    sortOrder: 999,
-    isFinished: false,
-    tokenDecimals: 18,
-    isBush: true,
-    isLp: false,
-    bushVersion: 1,
-    getUrl: '/#/swap?outputCurrency=0x56eab07247e3e6404ac90140F20bba61375d5C3C',
-  },
+  // {
+  //   sousId: 68, // V1
+  //   tokenName: 'WBNB',
+  //   tokenLabel: 'WBNB v1',
+  //   tokenAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+  //   stakingTokenName: QuoteToken.TAPS,
+  //   stakingTokenAddress: '0x56eab07247e3e6404ac90140F20bba61375d5C3C',
+  //   stakingTokenDecimals: 18,
+  //   contractAddress: {
+  //     97: '',
+  //     56: '0x705993A5576c67Ba7d0d1B8a39857AeB28712201',
+  //   },
+  //   poolCategory: PoolCategory.CORE,
+  //   projectLink: 'https://www.binance.com',
+  //   harvest: true,
+  //   tokenPerBlock: '0.00001388888889',
+  //   sortOrder: 999,
+  //   isFinished: false,
+  //   tokenDecimals: 18,
+  //   isBush: true,
+  //   isLp: false,
+  //   bushVersion: 1,
+  //   getUrl: '/#/swap?outputCurrency=0x56eab07247e3e6404ac90140F20bba61375d5C3C',
+  // },
   // {
   //   sousId: 69, // V1
   //   tokenName: 'BANANA',
